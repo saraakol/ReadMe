2f2032d27c5e7c5b2d37edd5d6c0045bc8abbd23
Andrej <andrejjokic00@gmail.com>
2021-05-10 18:36:43 +0200

    Inicijalizacija projekta

---
+++ Implementacija/public/js/skripta.js
@@ -0,0 +1,56 @@
+
+function kliknutalistawant() {
+    let checkBox = document.getElementById("wantlist");
+
+    
+    if (checkBox.checked == false) {
+        $(".wantlist").hide();
+    } else {
+        $(".wantlist").show();
+    }
+}
+
+function kliknutalisaall() {
+    let checkBox = document.getElementById("alllist");
+
+    
+    if (checkBox.checked == false) {
+        $(".alllist").hide();
+    } else {
+        $(".alllist").show();
+    }
+}
+
+function kliknutalistaread() {
+    let checkBox = document.getElementById("readlist");
+
+    
+    if (checkBox.checked == false) {
+        $(".readlist").hide();
+    } else {
+        $(".readlist").show();
+    }
+}
+
+function kliknutalistarecom() {
+    let checkBox = document.getElementById("recomlist");
+
+    
+    if (checkBox.checked == false) {
+        $(".recomlist").hide();
+    } else {
+        $(".recomlist").show();
+    }
+}
+
+function kliknutalistagenre() {
+    let checkBox = document.getElementById("genrelist");
+
+    
+    if (checkBox.checked == false) {
+        $(".genrelist").hide();
+    } else {
+        $(".genrelist").show();
+    }
+}
+
