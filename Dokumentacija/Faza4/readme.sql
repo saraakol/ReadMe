404398e1c3e51540df0fe224ff288caee74811cd
Andrej <andrejjokic00@gmail.com>
2021-05-17 20:01:18 +0200

    nista bitno

--- Dokumentacija/Faza4/readme.sql
+++ Dokumentacija/Faza4/readme.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `readme`
 --
-
+CREATE DATABASE IF NOT EXISTS `readme` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `readme`;
 -- --------------------------------------------------------
 
 --
