9e9e3905fca90a8de41844a46db112d8cf772fba
Andrej <andrejjokic00@gmail.com>
2021-05-25 23:57:28 +0200

    Nista spec

--- Implementacija/app/Controllers/Korisnik.php
+++ Implementacija/app/Controllers/Korisnik.php
@@ -150,44 +150,6 @@ class Korisnik extends BaseController {
         return $this->prikaz('Knjiga', ['knjiga' => $book]);
     }
 
-
-
-
-     /*
-     * Funkcija za dodavanje na All listu
-     * Nikola Krstic 18/0546
-     */
-
-    public function dodajNaReadListu() {
-        $user = $this->doctrine->em->getRepository(Entities\User::class)->findOneBy(["idu" => session()->get("korisnik")->getIdu()]);
-        $book = $this->doctrine->em->getRepository(Entities\Book::class)->findOneBy(["idb" => $this->request->getVar('idb')]);
-        
-        #foreach($user->getBooks() as $userbooktmp){
-           # if($userbooktmp->getIdb()->getIdb()==$book->getIdb()){
-            #    if($userbooktmp->getType()=="want-to-read"){
-             #       $this->doctrine->em->remove($userbooktmp);
-              #  }
-            #}
-        #}
-        $userbook = new Entities\Userbooks();
-        $userbook->setType("read");
-        $userbook->setIdb($book);
-        $userbook->setIdu($user);
-        
-        
-        $user->addBooks($userbook);
-        $this->doctrine->em->persist($userbook);
-        $this->doctrine->em->persist($user);
-        $this->doctrine->em->persist($book);
-        $this->doctrine->em->flush();
-        return $this->prikaz('Knjiga', ['knjiga' => $book]);
-    }
-	
-  
-
-    
-
-
     /*
      * komentarisanje knjige
      * Andrej Veselinovic 2018/0221
