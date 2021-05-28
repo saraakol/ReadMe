c70e11f8c63b633d5b5c30f865f943d6a2d478fe
Andrej <andrejjokic00@gmail.com>
2021-05-28 12:55:42 +0200

    Dodate slike u prikazu zahteva

--- Implementacija/app/Views/Stranice/Zahtevi.php
+++ Implementacija/app/Views/Stranice/Zahtevi.php
@@ -12,14 +12,19 @@
         <?php
         foreach ($korisnici as $korisnik) {
             echo "<div class='row buttons'>";
-            echo "  <div class='col-lg-3 col-md-6' style='background-color: #ffeedf; text-align: center;'>";
-            echo "      <a href=''><p><font>{$korisnik->getUsername()}</font></p></a>";
+            echo "  <div class='col-lg-3 col-md-6 zahtev' style='background-color: #ffeedf; text-align: center;'>";
+            if ($korisnik->getImage() == null) {
+                echo '  <img src="\images\users\no_photo.jpg" class="mr-3" alt="No photo">';
+            } else {
+                echo '  <img src="\images\users\\' . $korisnik->getIdu() . '.jpg" class="mr-3" alt="No photo">';
+            }
+            echo        "<h3>{$korisnik->getUsername()}</h3>";
             echo "  </div>";
             echo "  <div class='col-lg-5 col-md-1' style='background-color: #ffeedf;'>&nbsp;</div>";
             echo "  <div class='col-lg-4 col-md-5' style='background-color: #ffeedf;text-align: center;padding-bottom: 1vh;'>";
             echo "      <form name='potvrdiRegistraciju' method='get' action='" . site_url("Administrator/accept" . "$zahtev") . "'>";
             echo "          <input type='hidden' name='username' value='" . $korisnik->getUsername() . "'>";
-            echo "          <input type='submit' name='potvrdiRegistraciju' value='Accept'>";
+            echo "          <input class='zahtev-forma' type='submit' name='potvrdiRegistraciju' value='Accept'>";
             echo "      </form>";
             echo "      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
             echo "      <form name='odbijRegistraciju' method='get' action='" . site_url("Administrator/decline" . "$zahtev") . "'>";
