53fc3198a479f9960252d079828ed61120a7a415
Andrej <andrejjokic00@gmail.com>
2021-06-02 13:56:37 +0200

    Popravljeno prijavljivanje sebe

--- Implementacija/app/Views/Stranice/Knjiga.php
+++ Implementacija/app/Views/Stranice/Knjiga.php
@@ -156,7 +156,9 @@
                     echo '  <div class="media-body">';
                     echo '      <h4 class="mt-0">' . $komentar->getUser()->getUsername() . '</h4>';
                     if ($controller == 'Privilegovani' || $controller == 'Administrator') {
-                        echo "  <button class='prijavaKorisnika {$komentar->getUser()->getIdu()}' value='" . site_url("$controller/prijaviKorisnika/" . $komentar->getUser()->getIdu()) . "'>Report user</button>";
+                        if (session()->get('korisnik')->getIdu() != $komentar->getUser()->getIdu()) {
+                            echo "  <button class='prijavaKorisnika {$komentar->getUser()->getIdu()}' value='" . site_url("$controller/prijaviKorisnika/" . $komentar->getUser()->getIdu()) . "'>Report user</button>";
+                        }
                     }
                     echo '      <p class="komentar">' . $komentar->getText() . '</p>';
                     echo '  </div>';
