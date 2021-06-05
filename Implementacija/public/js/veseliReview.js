36be141e697e576072251dddca0a4b1e436a143e
Sara Kolarevic <kolarevic10@gmail.com>
2021-06-05 19:56:11 +0200

    unos praznog citata

--- Implementacija/public/js/veseliReview.js
+++ Implementacija/public/js/veseliReview.js
@@ -9,3 +9,13 @@ function checkAndSubmit(){
     }
 }
 
+function checkpom(){
+    if(document.getElementById("quotetext").value.trim()==="")
+    {
+        alert("Cannot submit empty quotes");
+    }
+    else
+    {
+        document.getElementById("formaQuote").submit();
+    }
+}
